aa1ed1c3024b7a89e78718f939aeeda3dba18378
jzzzzzzzzzzzzzzzz <97607676+jzzzzzzzzzzzzzzzz@users.noreply.github.com>
2023-11-04 16:07:04 +0000

    zz

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,21 +27,48 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
+// extern "C" {
 
-extern "Rust" {
+//     fn my_demo_function(a: u32) -> u32;
+//     #[link_name = "my_demo_function"] // 添加链接名称注解
+//     fn my_demo_function_alias(a: u32) -> u32;
+// }
 
-    fn my_demo_function(a: u32) -> u32;
+// mod foo {
+//     // No `extern` equals `extern "Rust"`.
+//     fn my_demo_function(a: u32) -> u32 {
+//         a
+//     }
+// }
 
-    #[link_name = "my_demo_function"]
-    fn my_demo_function_alias(a: u32) -> u32;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_success() {
+//         // The externally imported functions are UNSAFE by default
+//         // because of untrusted source of other languages. You may
+//         // wrap them in safe Rust APIs to ease the burden of callers.
+//         //
+//         //my_demo_function(123);
+//         // SAFETY: We know those functions are aliases of a safe
+//         // Rust function.
+//         unsafe {
+//             my_demo_function(123);
+//             my_demo_function_alias(456);
+//         }
+//     }
+// }
+
+#[no_mangle] // 添加 no_mangle 属性
+pub extern "C" fn my_demo_function(a: u32) -> u32 {
+    a
 }
 
-mod foo {
-    // No `extern` equals `extern "Rust"`.
-    fn my_demo_function(a: u32) -> u32 {
-        a
-    }
+extern "C" {
+    #[link_name = "my_demo_function"]
+    fn my_demo_function_alias(a: u32) -> u32;
 }
 
 #[cfg(test)]
@@ -50,16 +77,10 @@ mod tests {
 
     #[test]
     fn test_success() {
-        // The externally imported functions are UNSAFE by default
-        // because of untrusted source of other languages. You may
-        // wrap them in safe Rust APIs to ease the burden of callers.
-        //
-        //my_demo_function(123);
-        // SAFETY: We know those functions are aliases of a safe
-        // Rust function.
         unsafe {
             my_demo_function(123);
             my_demo_function_alias(456);
         }
     }
 }
+
